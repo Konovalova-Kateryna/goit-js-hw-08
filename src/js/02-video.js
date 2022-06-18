e8d6ad1f7bf300d5922854ce58f552071452b385
Konovalova-Kateryna <kytana2112@gmail.com>
2022-06-18 23:48:21 +0300

    task2_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_TIME = 'videoplayer-current-time';
-startTime = localStorage.getItem(STORAGE_TIME) || 0;
+const startTime = localStorage.getItem(STORAGE_TIME) || 0;
 console.log(startTime);
 
 player.on('timeupdate', throttle(onPause, 1000));
@@ -15,4 +15,16 @@ function onPause({seconds}) {
     startTime = localStorage.getItem(STORAGE_TIME);
 }
 
-player.setCurrentTime(startTime);
+player.setCurrentTime(startTime).then(function() {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
