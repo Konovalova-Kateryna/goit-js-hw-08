a7da9c018e76ec74df9a962df35c70d5b59093cc
Konovalova-Kateryna <kytana2112@gmail.com>
2022-06-18 23:39:05 +0300

    all

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,16 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const STORAGE_TIME = 'videoplayer-current-time'
-    
+const STORAGE_TIME = 'videoplayer-current-time';
+startTime = localStorage.getItem(STORAGE_TIME) || 0;
+console.log(startTime);
+
 player.on('timeupdate', throttle(onPause, 1000));
 
 function onPause({seconds}) {
     console.log(seconds);
     localStorage.setItem(STORAGE_TIME, seconds);
+    startTime = localStorage.getItem(STORAGE_TIME);
 }
-player.setCurrentTime(localStorage.getItem(STORAGE_TIME));
 
+player.setCurrentTime(startTime);
